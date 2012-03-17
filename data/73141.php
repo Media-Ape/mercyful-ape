47c219b72f1ed28f71dc68f340811e2f86978c2f
Brett Wandel <brett@ubuntu.(none)>
2012-03-17 06:01:31 -0700

    completely rewrote hook.php; now uses getEpisode and getSeason functions from /data/{series_id}.php to correct episode and season numbers.

---
+++ data/73141.php
@@ -0,0 +1,12 @@
+<?php
+function getEpisode($s,$e){
+ if ($s == 2) {return $e + 7;}
+ else {return $e;}
+}
+
+function getSeason($s, $e){
+ if ($s == 2) {return 1;}
+ else if ($s > 2) {return $s - 1;}
+ else {return $s;}
+}
+?>
