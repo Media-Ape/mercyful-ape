3e361860b5cd39bcb40af62f8e37bc5d44c54cb2
Brett Wandel <brett@ubuntu.(none)>
2012-03-17 11:00:09 -0700

    hopfully fixed american dad naming

--- data/73141.php
+++ data/73141.php
@@ -1,13 +1,13 @@
 <?php
 # American Dad!!!!
 function getEpisode($s,$e){
- if ($s == 1) {return $e + 7;}
+ if ($s == 2) {return $e + 7;}
  else {return $e;}
 }
 
 function getSeason($s, $e){
- if ($s == 1) {return 1;}
- else if ($s > 1) {return $s - 1;}
+ if ($s == 2) {return 1;}
+ else if ($s > 2) {return $s - 1;}
  else {return $s;}
 }
 ?>
