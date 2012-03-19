b0ddbe256486d9e38774d6ca638767329ecd1870
Brett Wandel <brett.wandel@live.com.au>
2012-03-19 12:27:30 -0700

    Should have all but Season 2 working for Pawn Stars

--- data/111051.php
+++ data/111051.php
@@ -1,9 +1,29 @@
 <?php include("tools.php");
 # Pawn Stars
+# on nzb.su season 4 starts at episode 40, and there is no season 5.
 
 function convert($v){
-  $tvdb = array(0,14,111,30);
-  $scene = array(0,14,36,67,53);
-  return episode(absolute($v,$tvdb),$scene);
+	$tvdb = array(1,14,111,30);
+	$a = absolute($v, $tvdb);
+
+	if ($a == 1) {return array("season" => 2,"episode" => 0);}
+	if ($v["season"] == 1) {return $v;}
+
+	#season 2 is stuffed....
+	if ($v["season"] == 2 and $v["episode"] <= 31) {return $v;}
+
+	# Fix for Mile High/Patriot Games Ordering
+	if ($a == 127) {return array("season" => 4,"episode" => 54);}
+	if ($a == 128) {return array("season" => 4,"episode" => 53);}
+	
+	if ($a > 45 AND $a < 113) { 	# Season 3
+		$v["season"]= 3;
+		$v["episode"] = $a - 46;
+	} else if ($a > 112) { 			#Season 4
+		$v["season"] = 4;
+		$v["episode"] = $a - 74;
+	}
+	return $v;
 }
+
 ?>
