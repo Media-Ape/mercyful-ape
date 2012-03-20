872d3a93794c04cd24a7eee5e6e8874be0ee3ca0
Brett Wandel <brett.wandel@live.com.au>
2012-03-20 07:21:04 -0700

    hopfully fixed the issue with S4E39.

--- data/111051.php
+++ data/111051.php
@@ -19,10 +19,10 @@ function convert($v){
 	if ($a == 127) {return array("season" => 4,"episode" => 54);}
 	if ($a == 128) {return array("season" => 4,"episode" => 53);}
 	
-	if ($a > 45 AND $a < 113) { 	# Season 3
+	if ($a > 45 AND $a < 114) { 	# Season 3
 		$v["season"]= 3;
 		$v["episode"] = $a - 46;
-	} else if ($a > 112) { 			#Season 4
+	} else if ($a > 113) { 			#Season 4
 		$v["season"] = 4;
 		$v["episode"] = $a - 74;
 	}
